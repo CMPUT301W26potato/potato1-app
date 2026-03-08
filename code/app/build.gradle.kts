9d4e5a6aa9967b93522cb51b95ffdf1cd605508b
“Viktoria <112983433+viktorialysenko@users.noreply.github.com>
2026-03-07 22:17:02 -0700

    Connected Firebase, added joinWaitlist, leaveWaitlist functionality. Adding two firestore collections: events, waitlist_entries

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Firebase BOM (manages versions for all Firebase libs)
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    // Firestore
+    implementation("com.google.firebase:firebase-firestore")
 }
