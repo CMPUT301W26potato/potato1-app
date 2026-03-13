008bf615d34e5205efeb34ce628d48b3c8a78437
Karina-Zhangg <xz30@ualberta.ca>
2026-03-13 16:59:10 -0600

    Added GUI changes to match figma with some additional button shape and colour changes
    Added a view that shows the QR code as well as the share options (saves QR code to gallery)
    
    Added the "Manage Event" View for organiser, buttons are not yet funcational other than the "edit event"
    
    Added the "edit event" button (Functionality: brings user back to event creation page with previous inputs saved, and the option to edit any information including banner image selected for upload)
    
    I deleted the fireserver connection to storage based on pricing plan limitations.
    
    Added working statuses to show the listing status of either open or closed based on the registration deadline that the organiser set upon event creation.

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
+
+// Force one protobuf version for app and test APKs (fixes NoSuchMethodError: registerDefaultInstance).
+subprojects {
+    afterEvaluate {
+        configurations.all {
+            resolutionStrategy {
+                force("com.google.protobuf:protobuf-javalite:3.25.3")
+                force("com.google.protobuf:protobuf-java:3.25.3")
+            }
+        }
+    }
+}
